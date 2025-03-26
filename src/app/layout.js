ad1035f3b7b44ad05e5fb314f12caaae91a6baa7
rambalpady <rajnikant.ambalpady@sony.com>
2025-03-26 09:57:04 -0700

    Add back Umami analytics with improved configuration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className} bg-light dark:bg-dark w-full min-h-screen`}>
         {children}
+        <Script
+          src="https://analytics.rajpoojary.com/script.js"
+          data-website-id="b5c5c0c0-5c5c-5c5c-5c5c-5c5c5c5c5c5c"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
